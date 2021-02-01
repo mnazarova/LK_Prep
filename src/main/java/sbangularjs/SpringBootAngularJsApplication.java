9fef9392d8427eb2705d75045f324f2c71ec4cb4
mnazarova <mnazarova@fkr-spb.ru>
2021-02-02 00:58:37 +0300

    added TomcatServerLocal

--- src/main/java/sbangularjs/SpringBootAngularJsApplication.java
+++ src/main/java/sbangularjs/SpringBootAngularJsApplication.java
@@ -2,11 +2,18 @@ package sbangularjs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class SpringBootAngularJsApplication {
+public class SpringBootAngularJsApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootAngularJsApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootAngularJsApplication.class);
     }
 }
